aa9608dacff597542adcfdbf16d2b5a660858cd5
vade <vade@vade.info>
2021-08-13 15:28:46 -0400

    update hsl math to match degree, percent percent format

--- colorgram/colorgram.py
+++ colorgram/colorgram.py
@@ -242,7 +242,7 @@ def hsl(r, g, b):
             h = (r - g) * 255 // diff + 1020
         h //= 6
     
-    return h, s, l
+    return (h/255.0 * 360.0), (s/255.0 * 100.0), (l/255.0 * 100.0)
 
 # Useful snippet for testing values:
 # print "Pixel #{}".format(str(y * width + x))
